bd803829eadb499d961d8ff08b88b21f4d281f47
Sergiusz <kuchko.02@gmail.com>
2022-11-15 22:11:57 +0300

    Startapp TODO

--- config/urls.py
+++ config/urls.py
@@ -18,10 +18,13 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from users.views import CustomUserModelViewSet
+from todo.views import ProjectModelViewSet, ToDoModelViewSet
 
 
 router = DefaultRouter()
 router.register('users', CustomUserModelViewSet)
+router.register('projects', ProjectModelViewSet)
+router.register('todo', ToDoModelViewSet)
 
 
 urlpatterns = [
