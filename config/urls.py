b129f3c453ee16ccf636a302c803fe53a327b15a
Sergiusz <kuchko.02@gmail.com>
2022-11-18 15:54:17 +0300

    Views, filtering, pagination

--- config/urls.py
+++ config/urls.py
@@ -17,12 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from users.views import CustomUserModelViewSet
+from users.views import CustomUserAPIView
 from todo.views import ProjectModelViewSet, ToDoModelViewSet
 
 
 router = DefaultRouter()
-router.register('users', CustomUserModelViewSet)
+# router.register('users', CustomUserModelViewSet)
 router.register('projects', ProjectModelViewSet)
 router.register('todo', ToDoModelViewSet)
 
@@ -31,4 +31,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include(router.urls)),
+    path('api/get_users', CustomUserAPIView.as_view())
 ]
--- users/views.py
+++ users/views.py
@@ -1,10 +1,44 @@
-from django.shortcuts import render
+from django.http import Http404
+from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer
+from rest_framework.response import Response
+from rest_framework.status import HTTP_204_NO_CONTENT, HTTP_400_BAD_REQUEST
+from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 
 from .models import CustomUser
 from .serializers import CustomUserModelSerializer
 
 
-class CustomUserModelViewSet(ModelViewSet):
-    queryset = CustomUser.objects.all()
-    serializer_class = CustomUserModelSerializer
+class CustomUserAPIView(APIView):
+    renderer_classes = [JSONRenderer, BrowsableAPIRenderer]
+
+    def get(self, request, format=None):
+        users = CustomUser.objects.all()
+        serializer = CustomUserModelSerializer(users, many=True)
+        return Response(serializer.data)
+
+    def get_object(self, pk):
+        try:
+            return CustomUser.objects.get(pk=pk)
+        except Exception:
+            raise Http404
+
+    def details(self, request, pk, format=None):
+        user = self.get_object(pk)
+        serializer = CustomUserModelSerializer(user)
+        return Response(serializer.data)
+
+    def patch(self, request, user_id):
+        user = CustomUser.objects.get(id=user_id)
+
+        serializer = CustomUserModelSerializer(user, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=HTTP_204_NO_CONTENT)
+
+        return Response(status=HTTP_400_BAD_REQUEST)
+
+
+# class CustomUserModelViewSet(ModelViewSet):
+#     queryset = CustomUser.objects.all()
+#     serializer_class = CustomUserModelSerializer
