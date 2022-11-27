3040b4f5de4a87dd5bdbda81d37f007bf33ae7f4
Sergiusz <kuchko.02@gmail.com>
2022-11-27 13:45:38 +0300

    Permissions. Auth

--- config/settings.py
+++ config/settings.py
@@ -144,4 +144,11 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     # 'PAGE_SIZE': 100,
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+    ],
+
 }
